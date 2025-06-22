fd51678da32c1f0cec93ee7199f981ab494af41c
Evan He <eh4359@rit.edu>
2025-06-22 18:27:38 -0400

    fixed atmosphere fresnel shader problem

--- src/components/space/earth/shaders/atmosphereFragment.glsl
+++ src/components/space/earth/shaders/atmosphereFragment.glsl
@@ -1,7 +1,16 @@
-varying vec3 vertexNormal;
+// varying vec3 vertexNormal;
+
+// void main() {
+//     // float intensity = pow(0.8 - dot(vertexNormal, vec3(0.0, 0.0, 1.0)), 2.0);
+//     float intensity = pow(0.8 - dot(vertexNormal, normalize(cameraPosition)), 2.0);
+//     gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * intensity;
+// }
+
+varying vec3 vNormal;
+varying vec3 vViewDir;
 
 void main() {
-    float intensity = pow(0.8 - dot(vertexNormal, vec3(0.0, 0.0, 1.0)), 2.0);
-    // gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * intensity;
-    gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * intensity;
+    float fresnel = pow(0.5 - dot(normalize(vNormal), normalize(vViewDir)), 6.0);
+    vec3 baseColor = vec3(0.3, 0.6, 1.0);
+    gl_FragColor = vec4(baseColor * fresnel, 1.0);
 }
--- src/components/space/earth/shaders/atmosphereVertex.glsl
+++ src/components/space/earth/shaders/atmosphereVertex.glsl
@@ -1,7 +1,18 @@
-varying vec3 vertexNormal;
+// varying vec3 vertexNormal;
+
+// void main() {
+//     vertexNormal = normalize(normalMatrix * normal);
+
+//     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+// }
+
+varying vec3 vNormal;
+varying vec3 vViewDir;
 
 void main() {
-    vertexNormal = normalize(normalMatrix * normal);
+    vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+    vViewDir = normalize(cameraPosition - worldPosition.xyz);
+    vNormal = normalize(mat3(modelMatrix) * normal);
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
