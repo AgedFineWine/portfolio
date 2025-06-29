392e82b3d0e038acc5503b716ff45584fe47fa9f
Evan He <eh4359@rit.edu>
2025-06-29 17:07:42 -0400

    about work section

---
+++ src/components/AboutWork.tsx
@@ -0,0 +1,29 @@
+import SubHeader from "./SectionHeader";
+import me from "../assets/me.png";
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function AboutWork() {
+  return (
+    <section className="py-20 mb-50 mx-auto boundingBox">
+
+      <div className="grid grid-cols-2 justify-items-center aboutWork">
+        <div>
+          <SubHeader singleWord={'Intro'} shortTextBefore={'About My '} emphasis={'Works'} />
+          <p className="mt-8 text-center text-muted-grey font-inter font-normal text-lg">
+            I've put a lot of time and heart into brainstorming, planning, and developing these personal projects.
+            My hope is that they can bring some value to others while also giving me the chance to have some fun
+            while learning and growing.
+          </p>
+        </div>
+
+        <div className="flex justify-center w-1/2">
+          <img className="w-[350px] aspect-square" src={me} alt="A picture of me" />
+        </div>
+
+      </div>
+    </section>
+  );
+}
