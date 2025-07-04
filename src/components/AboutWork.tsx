e6f33e26d598fd65ad96a49dd0e9d999b15480ae
Evan He <eh4359@rit.edu>
2025-07-04 18:04:58 -0400

    fixed photo size

--- src/components/AboutWork.tsx
+++ src/components/AboutWork.tsx
@@ -12,15 +12,15 @@ export default function AboutWork() {
       <div className="grid grid-cols-2 justify-items-center aboutWork">
         <div>
           <SubHeader id={"aboutWork"} singleWord={'Intro'} shortTextBefore={'About My '} emphasis={'Works'} />
-          <p className="mt-8 text-center text-muted-grey font-inter font-normal text-lg">
+          <p className="mt-8 text-left text-muted-grey font-inter font-normal text-lg md:text-center">
             I've put a lot of time and heart into brainstorming, planning, and developing these personal projects.
             My hope is that they can bring some value to others while also giving me the chance to have some fun
             while learning and growing.
           </p>
         </div>
 
-        <div className="flex justify-center w-1/2">
-          <img className="w-[350px] aspect-square" src={me} alt="A picture of me" />
+        <div className="flex justify-center  ">
+          <img className="max-w-[350px] w-full aspect-square" src={me} alt="A picture of me" />
         </div>
 
       </div>
